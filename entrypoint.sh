50dfd5e7cda90bc743cf3e5a3936839d1aedb53f
Gilang Pratama <pratamapriadi96@gmail.com>
2022-09-05 23:11:10 +0700

    Test perubahan

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,24 +21,32 @@ echo $MESSAGE
 flag=$(echo $MESSAGE | awk '{print match($0,"FIRST")}')
 if [ $flag -gt 0 ]; then
     last_tag="0.1.0"
+    echo "Default tag: ${last_tag}"
 else
     last_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+    echo "Last tag: ${last_tag}"
 fi
 
-echo "Last tag: ${last_tag}"
+VERSI=$(echo $last_tag | grep -o '[^-]*$')
 
-VERSION=$(echo $last_tag | grep -o '[^-]*$')
+MAJOR=$(echo $VERSI | cut -d. -f1)
+MINOR=$(echo $VERSI | cut -d. -f2)
+PATCH=$(echo $VERSI | cut -d. -f3)
 
-major=$(echo $VERSION | cut -d. -f1)
-minor=$(echo $VERSION | cut -d. -f2)
-patch=$(echo $VERSION | cut -d. -f3)
-
-flag=$(echo $MESSAGE | awk '{print match($0,"#MAJOR")}')
+flag=$(echo $MESSAGE | awk '{print match($0,"PERUBAHAN")}')
 if [ $flag -gt 0 ]; then
-    echo "Major ${major}+1"
+    NEXT_MAJOR=$MAJOR+1
+    echo "Perubahan di versi major ${NEXT_MAJOR}"
 fi
 
-flag=$(echo $MESSAGE | awk '{print match($0,"#MINOR")}')
-if [ $flag -gt 0 ]; then
-    echo "Minor ${minor}+1"
-fi
+# flag=$(echo $MESSAGE | awk '{print match($0,"FITUR")}')
+# if [ $flag -gt 0 ]; then
+#     NEXT_MINOR=$MINOR+1
+#     echo "Perubahan di versi minor ${NEXT_MINOR}"
+# fi
+
+# flag=$(echo $MESSAGE | awk '{print match($0,"PERBAIKAN")}')
+# if [ $flag -gt 0 ]; then
+#     NEXT_PATCH=$PATCH+1
+#     echo "Perubahan di versi patch ${NEXT_PATCH}"
+# fi
